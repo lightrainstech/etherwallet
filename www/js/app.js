18efe175d18fc77141f04ac4426954432dd90a29
niksmac <nikhil@lightrains.com>
2016-02-09 09:17:16 +0530

    Initial Commit

---
+++ www/js/app.js
@@ -0,0 +1,117 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers'])
+
+.run(function($ionicPlatform, $rootScope, $ionicLoading) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+
+  $rootScope.apiBase = "https://etherchain.org/api";
+
+  $rootScope.show = function() {
+    $ionicLoading.show({
+      template: '<p>Loading...</p><ion-spinner></ion-spinner>'
+    });
+  };
+  $rootScope.hide = function(){
+    $ionicLoading.hide();
+  };
+
+
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+    .state('app', {
+    url: '/app',
+    abstract: true,
+    templateUrl: 'templates/menu.html',
+    controller: 'AppCtrl'
+  })
+
+  .state('app.checkbalance', {
+    url: '/checkbalance',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/checkbalance.html',
+        controller: 'ChkBalanceCtrl'
+      }
+    }
+  })
+
+  .state('app.price', {
+      url: '/price',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/price.html',
+          controller: 'PriceCtrl'
+        }
+      }
+    })
+    .state('app.statitics', {
+      url: '/statitics',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/statitics.html',
+          controller: 'StatiticsCtrl'
+        }
+      }
+    })
+
+    .state('app.credits', {
+      url: '/credits',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/credits.html',
+          controller: 'CreditCtrl'
+        }
+      }
+    })
+
+    .state('app.transactions', {
+      url: '/transactions',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/transactions.html',
+          controller: 'TransactionsCtrl'
+        }
+      }
+    })
+
+    .state('app.send', {
+      url: '/send',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/send.html',
+        }
+      }
+    })
+
+  .state('app.block', {
+    url: '/block/:playlistId',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/block.html',
+        controller: 'SingleBlockCtrl'
+      }
+    }
+  });
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/checkbalance');
+});
---
+++ www/js/controllers.js
@@ -0,0 +1,79 @@
+angular.module('starter.controllers', [])
+
+.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+
+  // Form data for the login modal
+  $scope.loginData = {};
+
+})
+
+.controller('StatiticsCtrl', function($scope, $http, $rootScope, $ionicLoading) {
+  $rootScope.show($ionicLoading);
+
+  $http.get($rootScope.apiBase + '/basic_stats').then(function(resp) {
+    $rootScope.hide($ionicLoading);
+    $scope.playlists = resp.data.data.blocks;
+    // For JSON responses, resp.data contains the result
+  }, function(err) {
+    console.error('ERR', err);
+    // err.status will contain the status code
+  })
+
+})
+
+.controller('ChkBalanceCtrl', function($scope, $stateParams, $http, $rootScope, $q, $ionicLoading) {
+
+  $scope.checkBalance = function(data){
+    $rootScope.show($ionicLoading);
+
+    // if ( typeof(data.ifSave) !== 'undefined') {
+    //   window.localStorage['ethaddr'] = data.ethaddr;
+    // }
+
+    $http.get($rootScope.apiBase + '/account/' + data.ethaddr).then(function(resp) {
+      $rootScope.hide($ionicLoading);
+      data.ethaddr = '';
+      if ( typeof(resp.data.data[0]) !== 'undefined') {
+        $scope.response = resp.data.data[0].balance + ' ETH';
+        $scope.error = '';
+      } else {
+        $scope.error = "Sorry, Not a valid Ether Account";
+      }
+    }, function(err) {
+      data.ethaddr = '';
+      $rootScope.hide($ionicLoading);
+      $scope.error = "Some error occured, please try again.";
+    })
+  }
+})
+
+.controller('PriceCtrl', function($scope, $http, $rootScope, $ionicLoading) {
+
+  $rootScope.show($ionicLoading);
+
+  $http.get($rootScope.apiBase + '/statistics/price').then(function(resp) {
+    $rootScope.hide($ionicLoading);
+    var lastThirty = resp.data.data.slice(0, 30);
+
+    $scope.items = lastThirty;
+
+    // For JSON responses, resp.data contains the result
+  }, function(err) {
+    console.error('ERR', err);
+    // err.status will contain the status code
+  })
+
+})
+
+
+.controller('CreditCtrl', function($scope, $http, $rootScope) {
+  // $scope.copyThis = function () {
+  //   $cordovaClipboard
+  //   .copy('0xc2593b43eef66488d45b014fc8f86830f08c48fd')
+  //   .then(function () {
+  //     $scope.copied = "Address Copied Successfully...";
+  //   }, function () {
+  //     $scope.copied = "Not able to copy...";
+  //   });
+  // }
+});
