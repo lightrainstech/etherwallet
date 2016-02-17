28736a4291c3e85b6bdae795baff83efbe79d0b6
niksmac <nikhil@lightrains.com>
2016-02-17 14:42:09 +0530

    Code Linting

--- www/js/app.js
+++ www/js/app.js
@@ -1,9 +1,3 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers', 'chart.js','ngStorage'])
 
 .run(function($ionicPlatform, $rootScope, $ionicLoading) {
@@ -39,56 +33,56 @@ angular.module('starter', ['ionic', 'starter.controllers', 'chart.js','ngStorage
   $ionicConfigProvider.tabs.position('bottom');
   $stateProvider
   $stateProvider
-     .state('tabs', {
-       url: "/tab",
-       abstract: true,
-       templateUrl: "templates/tabs.html"
-     })
-     .state('tabs.checkbalance', {
-       url: "/checkbalance",
-       views: {
-         'home-tab': {
-           templateUrl: "templates/checkbalance.html",
-           controller: 'ChkBalanceCtrl'
-         }
-       }
-     })
-     .state('tabs.price', {
-       url: "/price",
-       views: {
-         'price-tab': {
-           templateUrl: "templates/price.html",
-           controller: 'PriceCtrl'
-         }
-       }
-     })
-     .state('tabs.marketcap', {
-       url: "/marketcap",
-       views: {
-         'marketcap-tab': {
-           templateUrl: "templates/marketcap.html",
-           controller: 'MarketCtrl'
-         }
-       }
-     })
-     .state('tabs.credits', {
-       url: "/credits",
-       views: {
-         'credits-tab': {
-           templateUrl: 'templates/credits.html',
-           controller: 'CreditCtrl'
-         }
-       }
-     })
-     .state('tabs.send', {
-       url: "/send",
-       views: {
-         'send-tab': {
-           templateUrl: 'templates/send.html',
-           controller: 'sendCtrl'
-         }
-       }
-     });
+  .state('tabs', {
+    url: "/tab",
+    abstract: true,
+    templateUrl: "templates/tabs.html"
+  })
+  .state('tabs.checkbalance', {
+    url: "/checkbalance",
+    views: {
+      'home-tab': {
+        templateUrl: "templates/checkbalance.html",
+        controller: 'ChkBalanceCtrl'
+      }
+    }
+  })
+  .state('tabs.price', {
+    url: "/price",
+    views: {
+      'price-tab': {
+        templateUrl: "templates/price.html",
+        controller: 'PriceCtrl'
+      }
+    }
+  })
+  .state('tabs.marketcap', {
+    url: "/marketcap",
+    views: {
+      'marketcap-tab': {
+        templateUrl: "templates/marketcap.html",
+        controller: 'MarketCtrl'
+      }
+    }
+  })
+  .state('tabs.credits', {
+    url: "/credits",
+    views: {
+      'credits-tab': {
+        templateUrl: 'templates/credits.html',
+        controller: 'CreditCtrl'
+      }
+    }
+  })
+  .state('tabs.send', {
+    url: "/send",
+    views: {
+      'send-tab': {
+        templateUrl: 'templates/send.html',
+        controller: 'sendCtrl'
+      }
+    }
+  });
 
 
   // if none of the above states are matched, use this as the fallback
